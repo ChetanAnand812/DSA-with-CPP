c74987e32e30a0eaee15657eb0abaf07cfb001f6
ChetanAnand812 <chetananand812@gmail.com>
2022-08-28 21:06:35 +0530

    initial commit

--- Lec8_Switch_&_Functions/passbyvalue.cpp
+++ Lec8_Switch_&_Functions/passbyvalue.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+//  1
 void dummy(int n){
     n++;
     cout<<"N is "<<n<<endl;
@@ -16,3 +17,39 @@ int main(){
 
     return 0;
 }
+
+
+/*   2
+void update(int a){
+    a = a/2;
+}
+int main(){                       // output - 10
+    int a = 10;
+    update(a);
+    cout<<a<<endl;
+}
+*/
+
+/*   3
+int update(int a){
+    a = a-5;
+    return a;
+}
+int main(){                       // output - 15
+    int a = 15;
+    update(a);
+    cout<<a<<endl;
+}
+*/
+
+/*  4
+int update(int a){
+    int ans = a*a;
+    return ans;
+}
+int main(){                       // output - 196
+    int a = 14;
+    a = update(a);
+    cout<<a<<endl;
+}
+*/
