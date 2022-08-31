618f072dcd8f619520d5144e607ce0afed0b33b0
ChetanAnand812 <chetananand812@gmail.com>
2022-08-31 22:16:33 +0530

    initial commit

--- Lec9_Array/Max_and_Min_in_Array.cpp
+++ Lec9_Array/Max_and_Min_in_Array.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
 
+int getMin(int arr[], int n){
+    int min = INT8_MAX;
+
+    for(int i=0;i<n;i++){
+        if(arr[i] < min ){
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int getMax(int arr[], int n){
+    int max = INT8_MIN;
+
+    for(int i=0;i<n;i++){
+        if(arr[i] > max ){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
 int main(){
     int arr[100], size;
     cout<<"Enter size of Array"<<endl;
@@ -10,4 +32,6 @@ int main(){
         cin>>arr[i];
     }
     
+    cout<<"Maximum value is "<<getMax(arr, size)<<endl;
+    cout<<"Minimum value is "<<getMin(arr, size)<<endl;
 }
