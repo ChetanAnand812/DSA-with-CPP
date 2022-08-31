3b9d9a99ee8801a54370e9bda0af25cfdfbc9b1f
ChetanAnand812 <chetananand812@gmail.com>
2022-08-31 22:03:54 +0530

    initial commit

---
+++ Lec9_Array/Max_and_Min_in_Array.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[100], size;
+    cout<<"Enter size of Array"<<endl;
+    cin>>size;
+
+    for (int i = 0; i < size; i++){
+        cin>>arr[i];
+    }
+    
+}
