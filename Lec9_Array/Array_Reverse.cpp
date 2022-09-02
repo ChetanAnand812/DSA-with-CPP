af220b0ab1528a4591044801427cad60aada88ab
ChetanAnand812 <chetananand812@gmail.com>
2022-09-02 08:16:33 +0530

    initial commit

---
+++ Lec9_Array/Array_Reverse.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int n){
+
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int n){
+    cout<<"Reverse of Array is"<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[100], size;
+    cout<<"Enter size of array"<<endl;
+    cin>>size;
+
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+
+    reverse(arr, size);
+
+    printArray(arr, size);
+
+    return 0;
+}
--- Lec9_Array/sum_of_array.cpp
+++ Lec9_Array/sum_of_array.cpp
@@ -1,3 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int arraySum(int arr[], int n){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+
+
+int main(){
+    int arr[100], size;
+    cout<<"Enter size of array"<<endl;
+    cin>>size;
+
+    for(int i =0; i<size; i++){
+        cin>>arr[i];
+    }
+
+    cout<<"Sum of array is: "<<arraySum(arr, size)<<endl;
+}
+ 
